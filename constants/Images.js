631835554a7da1ce28318d8fbc285c2e587b943a
Jordan cheeks <cheeks.jordan@yahoo.com>
2020-12-06 16:01:23 -0800

    Initial Push

---
+++ constants/Images.js
@@ -0,0 +1,43 @@
+// local imgs
+const Onboarding = require("../assets/imgs/Onboarding1.jpg");
+const Logo = require("../assets/imgs/FinalLogoGPSmall.jpg");
+const TEST = require("../assets/imgs/giphy123.gif");
+const LogoOnboarding = require("../assets/imgs/argon-logo-onboarding.png");
+const ProfileBackground = require("../assets/imgs/profile-screen-bg.png");
+const RegisterBackground = require("../assets/imgs/register-bg.png");
+const Pro = require("../assets/imgs/getPro-bg.png");
+const ArgonLogo = require("../assets/imgs/argonlogo.png");
+const iOSLogo = require("../assets/imgs/ios.png");
+const androidLogo = require("../assets/imgs/android.png");
+// internet imgs
+
+const ProfilePicture = 'https://hmp.me/dg44';
+
+const Viewed = [
+  'https://images.unsplash.com/photo-1501601983405-7c7cabaa1581?fit=crop&w=240&q=80',
+  'https://images.unsplash.com/photo-1543747579-795b9c2c3ada?fit=crop&w=240&q=80',
+  'https://images.unsplash.com/photo-1551798507-629020c81463?fit=crop&w=240&q=80',
+  'https://images.unsplash.com/photo-1470225620780-dba8ba36b745?fit=crop&w=240&q=80',
+  'https://images.unsplash.com/photo-1503642551022-c011aafb3c88?fit=crop&w=240&q=80',
+  'https://hmp.me/dg46',
+];
+
+const Products = {
+  'View article': 'https://images.unsplash.com/photo-1501601983405-7c7cabaa1581?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=840&q=840',
+};
+
+export default {
+  Onboarding,
+    TEST,
+  Logo,
+  LogoOnboarding,
+  ProfileBackground,
+  ProfilePicture,
+  RegisterBackground,
+  Viewed,
+  Products,
+  Pro,
+  ArgonLogo,
+  iOSLogo,
+  androidLogo
+};
---
+++ constants/articles.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    title: 'Co-Ed Volleyball Event Scheduled at 5 - 7PM Today ',
+        image: 'https://hmp.me/dg5l',
+    cta: 'Join Now', 
+    horizontal: true
+    },
+    {
+        title: 'Local park in Sorrento community sees boost in park use …',
+        image: 'https://hmp.me/dg48',
+        cta: 'View article',
+        horizontal: true
+    },
+  {
+    title: 'Co-Ed Soccer Event scheduled at 6 - 8PM at Tumbleweed Park ',
+      image: 'https://hmp.me/dg49',
+    cta: 'Join Now'
+  },
+  {
+    title: 'How disc golf can improve mental health …',
+      image: 'https://hmp.me/dg47',
+    cta: 'View article' 
+  },
+  {
+    title: 'Softball is release at local desert breeze park click in article to sign up for new season …',
+      image: 'https://hmp.me/dg5a',
+    cta: 'View article' 
+  },
+  {
+    title: 'Tumbleweed Rec center annouces new open basketball hours from 7am to 10PM M-F …',
+      image: 'https://hmp.me/dg46',
+    cta: 'View article', 
+    horizontal: true
+  },
+];
